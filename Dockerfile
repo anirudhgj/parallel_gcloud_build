93114c001b810d52c95af4ccc89fa6e68169c783
anirudh.jamkhandi <anirudh.jamkhandi@STTM180115.local>
2020-09-13 19:32:30 +0900

    updated main file

---
+++ Dockerfile
@@ -0,0 +1,38 @@
+FROM python:3.7-stretch
+
+LABEL "com.github.actions.name"="Parallel Build and Publish images to GCS"
+LABEL "com.github.actions.icon"="upload-cloud"
+LABEL "com.github.actions.color"="purple"
+
+COPY . . 
+
+RUN chmod +x /entrypoint.sh
+
+RUN pip install google-cloud-storage==1.28.1
+RUN pip install google-cloud-bigquery==1.24.0
+
+RUN apt-get update; apt-get install curl -y ; apt-get install -y vim
+RUN apt-get install -y wget \
+  && rm -rf /var/lib/apt/lists/*
+
+# Installs google cloud sdk, this is mostly for using gsutil to export model.
+RUN wget -nv \
+    https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.tar.gz && \
+    mkdir /root/tools && \
+    tar xvzf google-cloud-sdk.tar.gz -C /root/tools && \
+    rm google-cloud-sdk.tar.gz && \
+    /root/tools/google-cloud-sdk/install.sh --usage-reporting=false \
+        --path-update=false --bash-completion=false \
+        --disable-installation-options && \
+    rm -rf /root/.config/* && \
+    ln -s /root/.config /config && \
+    # Remove the backup directory that gcloud creates
+    rm -rf /root/tools/google-cloud-sdk/.install/.backup
+
+# Path configuration
+ENV PATH $PATH:/root/tools/google-cloud-sdk/bin
+# Make sure gsutil will use the default service account
+RUN echo '[GoogleCompute]\nservice_account = default' > /etc/boto.cfg
+
+RUN ls /tmp/
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+import yaml
+import logging
+import sys
+import json 
+from datetime import datetime
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+
+def main():
+
+    logging.info("Started the process to build comtainers parallel")
+    logging.info("Authenticating") 
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.environ["INPUT_GOOGLE_APPLICATION_CREDENTIALS"]
+
+    print (os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
+
+    with open(os.environ["GOOGLE_APPLICATION_CREDENTIALS"]) as f:
+        sa_details = json.load(f)
+
+    os.system("gcloud auth activate-service-account {} --key-file={} --project={}".format(sa_details['client_email'],
+                                                                                          os.environ["GOOGLE_APPLICATION_CREDENTIALS"],
+                                                                                          sa_details['project_id']))
+
+    logging.info("The value of the VERSION_GITHUB_SHA is: {}".format(os.environ["INPUT_GIT_SHA"]))
+    git_sha = os.environ['INPUT_GIT_SHA']
+    project_name = os.environ['INPUT_GKE_PROJECT']
+    build_input = os.environ['INPUT_IMAGE_AND_DOCKERFILE_PATH']
+
+    build_arr = build_input.split(' ')
+    image_names = build_arr[::2]
+    dockerfile_paths = build_arr[1::2]
+
+    logging.info("submitting the processes to the background")
+
+    for i in range(len(build_arr)):
+        image = image_names[i]
+        dockerfile_path = dockerfile_path[i]
+        cmd = f"gcloud builds submit --tag gcr.io/{project_name}/{image}:${git_sha} {dockerfile_path} &"
+        os.system(cmd)        
+        os.system("sleep 1")
+    
+if __name__ == "__main__":
+    main()
+
+
+
